4fbc02de911eaa0c81268f83270ef7af6d45943e
Pavel Cheklin <tscheklin@gmail.com>
2021-03-08 16:32:34 +0300

    RT-39: обновил обработку ошибок

--- internal/pkg/user/update.go
+++ internal/pkg/user/update.go
@@ -13,19 +13,34 @@ type userUpdateForm struct {
 	ConfirmPassword string `json:"confirm_password"`
 }
 
+func (form userUpdateForm) isEmpty() bool {
+	return form.Email == "" && form.Username == "" && form.Password == "" && form.ConfirmPassword == ""
+}
+
 func (api *Handler) Update(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	decoder := json.NewDecoder(r.Body)
 	userToUpdate := userUpdateForm{}
 	if err := decoder.Decode(&userToUpdate); err != nil {
-		log.Printf("error while unmarshalling JSON")
+		log.Printf("Error while unmarshalling JSON")
 		http.Error(w, `{"error": "bad form"}`, http.StatusBadRequest)
 		return
 	}
-	updateUser(userToUpdate)
+	if userToUpdate.isEmpty() {
+		log.Printf("Empty form")
+		http.Error(w, `{"error": "empty form"}`, http.StatusBadRequest)
+		return
+	}
+
+	if err := updateUser(userToUpdate); err != nil {
+		log.Printf("Error while updating user")
+		http.Error(w, `{"error": "error while updating user"}`, http.StatusBadRequest)
+		return
+	}
 	// TODO handle session
 }
 
-func updateUser(userUpdateForm) {
+func updateUser(userUpdateForm) (err error ) {
 	// TODO handle data
+	return
 }
