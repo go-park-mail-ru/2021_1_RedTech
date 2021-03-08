3a3cf3c8c7a0e13694fcbb6100c7cb732041f63b
Pavel Cheklin <tscheklin@gmail.com>
2021-03-08 15:53:25 +0300

    RT-39: написал задачи до момента появления сессий

--- internal/pkg/user/update.go
+++ internal/pkg/user/update.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"encoding/json"
-<<<<<<< HEAD
 	"log"
 	"net/http"
 )
@@ -39,26 +38,4 @@ func (api *Handler) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// TODO handle session
-}
-
-func updateUser(userUpdateForm) (err error ) {
-	// TODO handle data
-	return
-=======
-	"net/http"
-)
-
-func (api *Handler) Update(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	decoder := json.NewDecoder(r.Body)
-	userToUpdate := User{}
-	if err := decoder.Decode(&userToUpdate); err != nil {
-		_, _ = w.Write([]byte("{}"))
-		return
-	}
-	updateUser(userToUpdate)
-}
-
-func updateUser(user User) {
->>>>>>> RT-39: добавил хендлер на обработку обновления пользователя
 }
