dd698957e8e4a9f2bcc3abf5b117857f6c395527
Pavel Cheklin <tscheklin@gmail.com>
2021-03-08 17:49:03 +0300

    RT-38: добавил заготовку для тестов

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Redioteka
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.7.0
+)
---
+++ internal/pkg/user/get_test.go
@@ -0,0 +1,14 @@
+package user
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGet(t *testing.T) {
+	require.Equal(t, true, true)
+}
+
+func TestMe(t *testing.T) {
+	require.Equal(t, true, true)
+}
