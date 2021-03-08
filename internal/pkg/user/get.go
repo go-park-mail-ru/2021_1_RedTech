dd7b15c53d8684b94b6a69b721f43fd8f43ec9b6
Pavel Cheklin <tscheklin@gmail.com>
2021-03-08 18:46:13 +0300

    RT-38: убрал обработку json на входе

--- internal/pkg/user/get.go
+++ internal/pkg/user/get.go
@@ -1,34 +1,15 @@
 package user
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 )
 
-type userGetForm struct {
-	Username string `json:"username"`
-}
-
-func (form userGetForm) isEmpty() bool {
-	return form.Username == ""
-}
-
 func (api *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	decoder := json.NewDecoder(r.Body)
-	userForm := new(userGetForm)
-	if err := decoder.Decode(userForm); err != nil {
-		log.Printf("Error while unmarshalling JSON: %s", err)
-		http.Error(w, `{"error":"bad form"}`, http.StatusBadRequest)
-		return
-	}
-	if userForm.isEmpty() {
-		log.Print("Empty form")
-		http.Error(w, `{"error":"empty form"}`, http.StatusBadRequest)
-		return
-	}
-	if err := getUser(userForm); err != nil {
+	userId := 10
+	// todo get Id
+	if err := getUser(userId); err != nil {
 		log.Printf("Error while getting user: %s", err)
 		http.Error(w, `{"error":"bad form"}`, http.StatusBadRequest)
 		return
@@ -36,11 +17,10 @@ func (api *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	// TODO session handling
 }
 
-func getUser(form *userGetForm) (err error) {
+func getUser(userId int) (err error) {
 	// TODO data handling
 	return
 }
-
 func (api *Handler) Me(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	if err := getMe(); err != nil {
@@ -51,9 +31,7 @@ func (api *Handler) Me(w http.ResponseWriter, r *http.Request) {
 	// TODO session handling
 }
 
-
 func getMe() (err error) {
 	// TODO data handling
 	return
 }
-
