309138b52a8c9d02b6ea33b668f7a188e6c5a00f
Pavel Cheklin <tscheklin@gmail.com>
2021-03-08 17:39:54 +0300

    RT-38 добавил обработчик на /me

--- internal/pkg/user/get.go
+++ internal/pkg/user/get.go
@@ -37,6 +37,23 @@ func (api *Handler) Get(w http.ResponseWriter, r *http.Request) {
 }
 
 func getUser(form *userGetForm) (err error) {
-	// TODO data handleing
+	// TODO data handling
 	return
 }
+
+func (api *Handler) Me(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	if err := getMe(); err != nil {
+		log.Printf("Error while getting user: %s", err)
+		http.Error(w, `{"error":"bad form"}`, http.StatusBadRequest)
+		return
+	}
+	// TODO session handling
+}
+
+
+func getMe() (err error) {
+	// TODO data handling
+	return
+}
+
--- internal/pkg/user/user.go
+++ internal/pkg/user/user.go
@@ -21,10 +21,6 @@ func (api *Handler) Signup(w http.ResponseWriter, r *http.Request) {
 func (api *Handler) Logout(w http.ResponseWriter, r *http.Request) {
 }
 
-
-func (api *Handler) Me(w http.ResponseWriter, r *http.Request) {
-}
-
 func (api *Handler) Avatar(w http.ResponseWriter, r *http.Request) {
 }
 
