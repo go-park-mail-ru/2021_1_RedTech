2f87cead3304e09aff4eb75705321e55b78d23fa
Pavel Cheklin <tscheklin@gmail.com>
2021-03-08 15:53:25 +0300

    RT-39: написал задачи до момента появления сессий

--- internal/pkg/user/update.go
+++ internal/pkg/user/update.go
@@ -2,19 +2,30 @@ package user
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
+
+type userUpdateForm struct {
+	Email           string `json:"email"`
+	Username        string `json:"username"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
+}
 
 func (api *Handler) Update(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	decoder := json.NewDecoder(r.Body)
-	userToUpdate := User{}
+	userToUpdate := userUpdateForm{}
 	if err := decoder.Decode(&userToUpdate); err != nil {
-		_, _ = w.Write([]byte("{}"))
+		log.Printf("error while unmarshalling JSON")
+		http.Error(w, `{"error": "bad form"}`, http.StatusBadRequest)
 		return
 	}
 	updateUser(userToUpdate)
+	// TODO handle session
 }
 
-func updateUser(user User) {
+func updateUser(userUpdateForm) {
+	// TODO handle data
 }
