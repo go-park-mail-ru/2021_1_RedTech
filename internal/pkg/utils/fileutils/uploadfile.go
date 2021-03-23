a52bcfe00ea49b2b9b20a32972b1a3ea52c689a4
Pavel Cheklin <tscheklin@gmail.com>
2021-03-23 20:54:24 +0300

    RT-75: подправил логи

--- internal/pkg/utils/fileutils/uploadfile.go
+++ internal/pkg/utils/fileutils/uploadfile.go
@@ -26,7 +26,6 @@ func UploadFile(r *http.Request, root, path, urlRoot string) (string, error) {
 	r.ParseMultipartForm(5 * 1024 * 1024)
 	uploaded, header, err := r.FormFile("avatar")
 	if err != nil {
-		log.Printf("Error while uploading file: %s", err)
 		return "", fmt.Errorf("error while parsing file: %s", err)
 	}
 	defer uploaded.Close()
@@ -35,16 +34,13 @@ func UploadFile(r *http.Request, root, path, urlRoot string) (string, error) {
 	log.Print("Created file with name ", filename)
 	file, err := createFile(root, path, filename)
 	if err != nil {
-		log.Printf("error while creating file: %s", err)
 		return "", fmt.Errorf("file createing error %s", err)
 	}
 	defer file.Close()
 
 	filename = urlRoot + path + filename
-	log.Print("avatar name ", filename)
 	_, err = io.Copy(file, uploaded)
 	if err != nil {
-		log.Printf("error while writing in file: %s", err)
 		return "", fmt.Errorf("copy error: %s", err)
 	}
 	return filename, nil
