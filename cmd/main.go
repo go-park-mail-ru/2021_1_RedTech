a0841fdb76839fc55e53ac475d06c2f3f13413e5
Pavel Cheklin <tscheklin@gmail.com>
2021-03-29 16:52:21 +0300

    RT-93: подправил main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"Redioteka/internal/pkg/domain"
-	"encoding/json"
-	"fmt"
-)
-
+import "Redioteka/internal/app/server"
 
 func main() {
-	//server.RunServer(":8081")
-	res, _ := json.Marshal(domain.Movie{
-		ID:          1,
-		Title:       "Film",
-		Description: "Test data",
-		Rating:      9,
-		Countries:   []string{"Japan", "South Korea"},
-		IsFree:      false,
-		Genres:      []string{"Comedy"},
-		Actors:      []string{"Sana", "Momo", "Mina"},
-		Avatar:      "/static/movies/default.jpg",
-		Type:        domain.MovieT,
-		Year:        "2012",
-		Director:    []string{"James Cameron"},
-	})
-	fmt.Println(string(res))
+	server.RunServer(":8081")
 }
