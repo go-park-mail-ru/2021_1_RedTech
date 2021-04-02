919db63be67bd5c162960c1d059a3574defd33ca
GregoryBS <aanonimov117@gmail.com>
2021-04-02 17:40:15 +0000

    RT-95: Функция для select-запросов.

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -11,19 +11,43 @@ type DBManager struct {
 	pool *pgxpool.Pool
 }
 
-func Connect() (*DBManager, error) {
-	connString := "user=redtech password=red_tech host=localhost port=5432 dbname=netflix"
-	pool, err := pgxpool.Connect(context.Background(), connString)
+func (db *DBManager) Query(queryString string) ([]interface{}, error) {
+	ctx := context.Background()
+	tx, err := db.pool.Begin(ctx)
 	if err != nil {
 		log.Log.Error(err)
 		return nil, err
 	}
+	defer tx.Rollback(ctx)
+
+	rows, err := tx.Query(ctx, queryString)
+	if err != nil {
+		log.Log.Error(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		log.Log.Error(err)
+		return nil, err
+	}
+	return rows.Values()
+}
+
+func Connect() *DBManager {
+	connString := "user=redtech password=red_tech host=localhost port=5432 dbname=netflix"
+	pool, err := pgxpool.Connect(context.Background(), connString)
+	if err != nil {
+		log.Log.Error(err)
+		return nil
+	}
 	log.Log.Info("Successful connection to postgres")
-	return &DBManager{pool: pool}, nil
+	return &DBManager{pool: pool}
 }
 
 func Disconnect() {
 	Manager.pool.Close()
 }
 
-var Manager, myErr = Connect()
+var Manager = Connect()
