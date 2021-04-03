ae7c58a1568c892b83ca1af7b5554b2d156e1b24
GregoryBS <aanonimov117@gmail.com>
2021-04-03 19:58:01 +0000

    RT-95: Исправление ошибок преобразования типов в репозитории юзеров.

--- internal/pkg/user/repository/db.go
+++ internal/pkg/user/repository/db.go
@@ -4,15 +4,14 @@ import (
 	"Redioteka/internal/pkg/database"
 	"Redioteka/internal/pkg/domain"
 	"errors"
-	"fmt"
 )
 
 type dbUserRepository struct {
 	db *database.DBManager
 }
 
-func NewUserRepository() domain.UserRepository {
-	return &dbUserRepository{db: database.Manager}
+func NewUserRepository(db *database.DBManager) domain.UserRepository {
+	return &dbUserRepository{db: db}
 }
 
 func (ur *dbUserRepository) GetById(id uint) (domain.User, error) {
@@ -20,26 +19,39 @@ func (ur *dbUserRepository) GetById(id uint) (domain.User, error) {
 	if err != nil {
 		return domain.User{}, err
 	}
-	first := data[0]
-	if first == nil {
+	first, ok := data[0].([]interface{})
+	if first == nil || !ok {
 		return domain.User{}, errors.New("User does not exist")
 	}
-	fmt.Println(first)
-	user := first.(domain.User)
+
+	user := domain.User{
+		ID:       uint(first[0].(int32)),
+		Username: first[1].(string),
+		Email:    first[2].(string),
+		Avatar:   first[3].(string),
+	}
 	return user, nil
 }
 
 func (ur *dbUserRepository) GetByEmail(email string) (domain.User, error) {
-	data, err := ur.db.Query("select id, username, email, avatar from users where email = $1", email)
+	data, err := ur.db.Query("select id, username, email, avatar, password from users where email = $1", email)
 	if err != nil {
 		return domain.User{}, err
 	}
-	first := data[0]
-	if first == nil {
+	first, ok := data[0].([]interface{})
+	if first == nil || !ok {
 		return domain.User{}, errors.New("User does not exist")
 	}
-	fmt.Println(first)
-	user := first.(domain.User)
+
+	var password [domain.HashLen]byte
+	copy(password[:], first[4].([]byte))
+	user := domain.User{
+		ID:       uint(first[0].(int32)),
+		Username: first[1].(string),
+		Email:    first[2].(string),
+		Avatar:   first[3].(string),
+		Password: password,
+	}
 	return user, nil
 }
 
@@ -48,12 +60,15 @@ func (ur *dbUserRepository) Update(user *domain.User) error {
 }
 
 func (ur *dbUserRepository) Store(user *domain.User) (uint, error) {
-	err := ur.db.Exec("insert into users values(default, $1, $2, $3, $4, false);", user.Username, user.Email, user.Password, user.Avatar)
+	data, err := ur.db.Query("insert into users values(default, $1, $2, $3, $4, false) returning id;", user.Username, user.Email, user.Password[:], user.Avatar)
 	if err != nil {
 		return 0, err
 	}
-	newUser, err := ur.GetByEmail(user.Email)
-	return newUser.ID, nil
+	first, ok := data[0].([]interface{})
+	if first == nil || !ok {
+		return 0, errors.New("Cannot create user in database")
+	}
+	return uint(first[0].(int32)), nil
 }
 
 func (ur *dbUserRepository) Delete(id uint) error {
