44bcac6b6be8c107ecdb5a970413b5d27cca79c8
Pavel Cheklin <tscheklin@gmail.com>
2021-04-04 22:42:22 +0300

    RT-86: убрал ненужный тест

--- internal/pkg/movie/delivery/http/http_test.go
+++ internal/pkg/movie/delivery/http/http_test.go
@@ -2,17 +2,7 @@ package http
 
 import (
 	"Redioteka/internal/pkg/domain"
-	"Redioteka/internal/pkg/movie"
-	mock2 "Redioteka/internal/pkg/movie/usecase/mock"
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"github.com/golang/mock/gomock"
-	"github.com/gorilla/mux"
-	"github.com/stretchr/testify/require"
 	"net/http"
-	"net/http/httptest"
-	"testing"
 )
 
 var movieTestData = map[uint]domain.Movie{
@@ -117,41 +107,3 @@ var movieGetTests = []movieTestCase{
 		status:   http.StatusOK,
 	},
 }
-
-func TestMovieHandler_Get(t *testing.T) {
-	res, _ := json.Marshal(movieTestData[1])
-	fmt.Println(string(res))
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mCaseMock := mock2.NewMockMovieUsecase(ctrl)
-	handler := &MovieHandler{
-		MUCase: mCaseMock,
-	}
-	for _, test := range movieGetTests {
-		t.Run(fmt.Sprintf("IN: %v, OUT: %v, CODE: %v", test.inURL, test.outJSON, test.status),
-			func(t *testing.T) {
-				test.outJSON += "\n"
-				body := bytes.NewReader([]byte(""))
-				r := httptest.NewRequest("GET", test.inURL, body)
-				r = mux.SetURLVars(r, test.inParams)
-				w := httptest.NewRecorder()
-
-				if test.status == http.StatusOK {
-					mCaseMock.EXPECT().GetById(uint(1)).Times(1).Return(movieTestData[1], nil)
-				} else if test.status == http.StatusNotFound {
-					mCaseMock.EXPECT().GetById(uint(2)).Times(1).Return(domain.Movie{}, movie.NotFoundError)
-				}
-
-				handler.Get(w, r)
-				current := movieTestCase{
-					inURL:    test.inURL,
-					inParams: test.inParams,
-					outMovie: test.outMovie,
-					outJSON:  w.Body.String(),
-					status:   w.Code,
-				}
-				require.Equal(t, current, test)
-			})
-	}
-}
-
