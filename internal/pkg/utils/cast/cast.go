adbf6aeb50ac47fd702cb3304ecd3943fcc0883e
GregoryBS <aanonimov117@gmail.com>
2021-04-05 14:22:04 +0000

    RT-95: Больше функций для перевода в байты и обратно.

--- internal/pkg/utils/cast/cast.go
+++ internal/pkg/utils/cast/cast.go
@@ -1,27 +1,60 @@
 package cast
 
 import (
+	"bytes"
 	"encoding/binary"
 	"math"
 )
 
-func ToString(bytes []byte) string {
-	return string(bytes)
+func ToString(bytesArg []byte) string {
+	return string(bytesArg)
 }
 
-func ToFloat(bytes []byte) float32 {
-	bits := binary.BigEndian.Uint32(bytes)
+func ToFloat(bytesArg []byte) float32 {
+	bits := binary.BigEndian.Uint32(bytesArg)
 	return math.Float32frombits(bits)
 }
 
-func ToUint(bytes []byte) uint {
-	return uint(binary.BigEndian.Uint32(bytes))
+func ToUint(bytesArg []byte) uint {
+	return uint(binary.BigEndian.Uint32(bytesArg))
 }
 
-func ToInt(bytes []byte) int {
-	return int(binary.BigEndian.Uint32(bytes))
+func ToInt(bytesArg []byte) int {
+	reader := bytes.NewReader(bytesArg)
+	num, _ := binary.ReadVarint(reader)
+	return int(num)
 }
 
-func ToSmallInt(bytes []byte) int {
-	return int(binary.BigEndian.Uint16(bytes))
+func ToSmallInt(bytesArg []byte) int {
+	reader := bytes.NewReader(bytesArg)
+	num, _ := binary.ReadVarint(reader)
+	return int(num)
+}
+
+func StrToBytes(arg string) []byte {
+	return []byte(arg)
+}
+
+func FloatToBytes(arg float32) []byte {
+	bytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(bytes, math.Float32bits(arg))
+	return bytes
+}
+
+func UintToBytes(arg uint) []byte {
+	bytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(bytes, uint32(arg))
+	return bytes
+}
+
+func IntToBytes(arg int) []byte {
+	bytes := make([]byte, 4)
+	binary.PutVarint(bytes, int64(arg))
+	return bytes
+}
+
+func SmallIntToBytes(arg int) []byte {
+	bytes := make([]byte, 8)
+	binary.PutVarint(bytes, int64(arg))
+	return bytes
 }
