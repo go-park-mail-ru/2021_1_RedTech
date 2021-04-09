c2f03cd6a185264b34276f15fef7fcf7d6b165b2
GregoryBS <aanonimov117@gmail.com>
2021-04-09 01:22:33 +0000

    RT-88: Скрипты на создание базы.

---
+++ scripts/sql/tables.sql
@@ -0,0 +1,91 @@
+drop table if exists users;
+create table users (
+	id serial not null primary key,
+	username varchar(64),
+	email varchar(64) unique,
+	password bytea,
+	avatar text,
+	is_donate bool default false
+);
+						 
+drop table if exists movie_types;
+create table movie_types (
+	id smallint not null primary key,
+	type varchar(64)
+);
+insert into movie_types values(1, 'movie'), (2, 'series');
+
+drop table if exists movies;
+create table movies (
+	id serial not null primary key,
+	title text,
+	description text,
+	avatar text,
+	rating real,
+	is_free boolean,
+	type smallint,
+	release_year smallint,
+	directors text,
+	countries text,
+	add_date date,
+	constraint to_type foreign key(type) references movie_types(id) on delete set null
+);						 
+						 
+drop table if exists genres;
+create table genres (
+	id serial not null primary key,
+	name varchar(64)
+);
+
+drop table if exists movie_genres;
+create table movie_genres (
+	id serial not null primary key,
+	movie_id int,
+	genre_id int,
+	constraint to_movie foreign key(movie_id) references movies(id) on delete cascade,
+	constraint to_genre foreign key(genre_id) references genres(id) on delete cascade
+);
+
+drop table if exists actors;
+create table actors (
+	id serial not null primary key,
+	firstname varchar(64),
+	lastname varchar(64)
+);
+
+drop table if exists movie_actors;
+create table movie_actors (
+	id serial not null primary key,
+	movie_id int,
+	actor_id int,
+	constraint to_movie foreign key(movie_id) references movies(id) on delete cascade,
+	constraint to_actor foreign key(actor_id) references actors(id) on delete cascade
+);
+
+drop table if exists user_favs;
+create table user_favs (
+	id serial not null primary key,
+	user_id int,
+	movie_id int,
+	constraint to_user foreign key(user_id) references users(id) on delete cascade,
+	constraint to_movie foreign key(movie_id) references movies(id) on delete cascade
+);
+
+drop table if exists movie_votes;
+create table movie_votes (
+	id serial not null primary key,
+	user_id int,
+	movie_id int,
+	value smallint,
+	constraint to_user foreign key(user_id) references users(id) on delete cascade,
+	constraint to_movie foreign key(movie_id) references movies(id) on delete cascade
+);
+
+drop table if exists movie_videos;
+create table movie_videos (
+	id serial not null primary key,
+	movie_id int,
+	path text,
+	duration int,
+	constraint to_movie foreign key(movie_id) references movies(id) on delete cascade
+);
