1524013114a41d89631c7366055923c0c4cc8fe6
GregoryBS <aanonimov117@gmail.com>
2021-05-23 20:33:51 +0000

    RT-150: интерфейсы для юзкейсов и репозитория подписок

---
+++ internal/pkg/domain/subscription.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"Redioteka/internal/pkg/subscription/delivery/grpc/proto"
+	"time"
+)
+
+type Subscription struct {
+	UserID    uint
+	Expiraton time.Time
+}
+
+//go:generate mockgen -destination=../subscription/repository/mock/mock_repo.go -package=mock Redioteka/internal/pkg/domain SubscriptionRepository
+type SubscriptionRepository interface {
+	Create(sub *Subscription) error
+	Delete(id uint) error
+}
+
+//go:generate mockgen -destination=../subscription/usecase/mock/mock_usecase.go -package=mock Redioteka/internal/pkg/domain SubscriptionUsecase
+type SubscriptionUsecase interface {
+	Create(form *proto.Payment) error
+	Delete(id uint) error
+}
