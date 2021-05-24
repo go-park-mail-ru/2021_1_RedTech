30fd785d2b5a05b4fdd8fcb18b7c1a1d98a27f41
GregoryBS <aanonimov117@gmail.com>
2021-05-24 23:09:21 +0000

    RT-68: новые условия для проверки cors

--- internal/pkg/middlewares/cors.go
+++ internal/pkg/middlewares/cors.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"Redioteka/internal/pkg/utils/jsonerrors"
 	"Redioteka/internal/pkg/utils/log"
 	"net/http"
 )
@@ -20,16 +21,20 @@ func (m *GoMiddleware) CORSMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		if _, found := whiteListOrigin[origin]; found {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		} else {
-			log.Log.Warn("Request from unknown host: " + origin)
+		if r.Method != http.MethodGet {
+			if _, found := whiteListOrigin[origin]; found {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			} else {
+				log.Log.Warn("Request from unknown host: " + origin)
+				http.Error(w, jsonerrors.JSONMessage("unknown origin"), http.StatusMethodNotAllowed)
+				return
+			}
 		}
 
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, "+
-			"Content-Language, Content-Type, Content-Encoding")
+			"Content-Language, Content-Type, Content-Encoding, X-CSRF-Token")
 		if r.Method == "OPTIONS" {
 			return
 		}
